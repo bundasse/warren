f7545a360bc3b1dd53bdbeb195a3b2022d0bff49
bundasse <ymtotolove@gmail.com>
2025-11-04 21:10:24 +0900

    046

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'main',
+      component: MainView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue'),
+    },
+    {
+      path: '/pic',
+      name: 'pic',
+      component: () => import('../views/PicView.vue'),
+    },
+    {
+      path: '/yarn',
+      name: 'yarn',
+      component: () => import('../views/YarnView.vue'),
+    },
+    {
+      path: '/review',
+      name: 'review',
+      component: () => import('../views/ReviewView.vue'),
+    },
+    {
+      path: '/link',
+      name: 'link',
+      component: () => import('../views/LinkView.vue'),
     },
   ],
 })
---
+++ src/views/MainView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+const comment = ref([{idx:1, text:'ssss', writer:'토끼파행동대장'}])
+</script>
+
+<template>
+  <div>
+    <div class="">
+      로고이미지 화면
+    </div>
+    <div class="">
+      방명록
+      <ul>
+        <li v-for="(e,i) in comment" :key="i">
+          <p>{{ e.writer }}</p>
+          <div>
+            {{ e.text }}
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
