5eb8ca450a6cd773bd78aeaeb7558e3d8ce5b576
bundasse <ymtotolove@gmail.com>
2025-11-13 22:33:17 +0900

    055

--- src/views/PicView.vue
+++ src/views/PicView.vue
@@ -1,6 +1,13 @@
 <script setup></script>
 <template>
     <div>
-        Pic
+        <div class="picNote">
+            <img src="" alt="" style="max-width: 500px;">
+            <div>
+                <div>닉네임</div>
+                <div>투고일시</div>
+                <div>코멘트</div>
+            </div>
+        </div>
     </div>
 </template>
