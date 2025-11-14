faa2888dd1103b909c697355252394839d579775
bundasse <ymtotolove@gmail.com>
2025-11-14 19:57:17 +0900

    056

--- src/views/PicView.vue
+++ src/views/PicView.vue
@@ -4,8 +4,13 @@
         <div class="picNote">
             <img src="" alt="" style="max-width: 500px;">
             <div>
-                <div>닉네임</div>
-                <div>투고일시</div>
+                <div>
+
+                    <div>닉네임</div>
+                    <div>투고일시</div>
+                    <button><i class="bx bx-edit"></i></button>
+                    <button><i class="bx bx-delete"></i></button>
+                </div>
                 <div>코멘트</div>
             </div>
         </div>
