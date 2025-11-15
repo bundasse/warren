7e6e6b348ed3c330c0cff97fdae521fd0458c6ef
bundasse <ymtotolove@gmail.com>
2025-11-15 19:40:33 +0900

    057

---
+++ src/components/PicWriteComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+const txtName= ref()
+const txtPassword = ref()
+const txtComments = ref()
+</script>
+
+<template>
+    <div>
+        <img src="" alt="" style="max-width: 500px;">
+        <div>
+            <div>
+                <div>
+                    <label for="txtPicName">이름</label>    
+                    <input type="text" id="txtPicName" v-model="txtName">
+                </div>
+                <div>
+                    <label for="txtPicPassword">비밀번호</label>
+                    <input type="password" id="txtPicPassword" v-model="txtPassword">
+                </div>
+            </div>
+            <div>코멘트</div>
+            <textarea name="" id="txtPicComments" v-model="txtComments"></textarea>
+            <div class="d-flex buttons">
+                <button>저장</button>
+                <button>취소</button>
+            </div>
+        </div>
+    </div>
+</template>
