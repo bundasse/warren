a98c050613e29e875193a4ef03995049917a3a7f
bundasse <ymtotolove@gmail.com>
2025-11-22 21:22:56 +0900

    064

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
-    This is an about page
+    <div class="profilePic">
+      <img src="" alt="">
+    </div>
+    <h3>다스 Dasse</h3>
+    <div class="profileContents">
+      TRPG
+    </div>
+    <div class="profileContents">
+      Knit+Crochet
+    </div>
+    <div class="profileContents">
+      Pic
+    </div>
   </div>
 </template>
 
 <style>
+.profilePic{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+}
 </style>
