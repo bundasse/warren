632f6523b9838a19651ca01d3bd2687b012596bb
bundasse <ymtotolove@gmail.com>
2025-11-25 21:45:05 +0900

    067

--- src/views/PicView.vue
+++ src/views/PicView.vue
@@ -1,22 +1,25 @@
 <script setup></script>
 <template>
     <div>
-        <div class="picNote">
-            <img src="" alt="" style="max-width: 500px;">
-            <div class="picNoteTextArea">
-                <div class="picNoteProfile">
-                    <div class="names">
-                        <div class="name">닉네임</div>
-                        <div class="writeTime">투고일시</div>
+        <template>
+            <div class="picNote">
+                <img src="" alt="" style="max-width: 500px;">
+                <div class="picNoteTextArea">
+                    <div class="picNoteProfile">
+                        <div class="names">
+                            <div class="name">닉네임</div>
+                            <div class="writeTime">투고일시</div>
+                        </div>
+                        <div class="buttons">
+                            <button><i class="bx bx-edit"></i></button>
+                            <button><i class="bx bx-delete"></i></button>
+                        </div>
                     </div>
-                    <div class="buttons">
-                        <button><i class="bx bx-edit"></i></button>
-                        <button><i class="bx bx-delete"></i></button>
-                    </div>
+                    <div>코멘트 </div>
+                    <p></p>
                 </div>
-                <div>코멘트 </div>
             </div>
-        </div>
+        </template>
     </div>
 </template>
 
