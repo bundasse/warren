58431939147bfa74ad9780e65bdd325877cc677d
bundasse <ymtotolove@gmail.com>
2025-12-04 21:51:53 +0900

    076

--- src/views/LinkView.vue
+++ src/views/LinkView.vue
@@ -23,6 +23,9 @@ function addBanner() {
         img : txtImgValue.value,
         link: txtLinkValue.value
     }
+
+    txtImgValue.value = '';
+    txtLinkValue.value = '';
 }
 </script>
 <template>
