1fb892f8f4a071f9587f75641d8c53bc324abbfa
bundasse <ymtotolove@gmail.com>
2025-12-29 00:19:11 +0900

    100

--- src/components/PicWriteComponent.vue
+++ src/components/PicWriteComponent.vue
@@ -10,7 +10,6 @@ function saveCommand() {
         password: txtPassword.value,
         comment: txtComments.value
     }
-    console.log(data)
 }
 function cancelCommand() {
     console.log('cancel');
